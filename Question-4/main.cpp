b3d55d1f1493bed52e8e6ba493d45a8905dc40f5
Ethan Leet <ethanjleet@gmail.com>
2022-05-20 10:09:21 +1000

    refactored

--- Question-4/main.cpp
+++ Question-4/main.cpp
@@ -8,21 +8,22 @@ using std::cout;
 void multiple_linear_regression(std::vector< std::vector<double>>& lhs, 
                                 std::vector<double>& rhs, std::vector<double>& d, 
                                 std::vector<double>& s, std::vector<double>& f);
-std::vector<double> gaussian_elimination(std::vector<std::vector<double>> lhs, 
-                                         std::vector<double> rhs);
+std::vector<double> gaussian_elimination(std::vector<std::vector<double>>& lhs, 
+                                         std::vector<double>& rhs);
+// used to fill multiple matrix values                                       
+void fill_matrix(std::vector<double>& row, const std::vector<double>& inc)
+{ for (int i = 0; i < inc.size(); i++) row[i] += inc[i]; }
 double F(double a, double b, double c, double d, double s)
 { return a * pow(d, b) * pow(s, c); }
-double F_log(double a, double b, double c, double d, double s)
-{ return log10(a) + b * d + c * s; }
 
 int main()
 {
     std::vector<std::vector<double>> lhs = {{0, 0, 0},
                                             {0, 0, 0},
                                             {0, 0, 0}};
-    std::vector<double> d = {log10(1), log10(2), log10(3), log10(1), log10(2), log10(3), log10(1), log10(2), log10(3)},
-                        s = {log10(0.001), log10(0.001), log10(0.001), log10(0.01), log10(0.01), log10(0.01), log10(0.05), log10(0.05), log10(0.05)},
-                        f = {log10(1.4), log10(8.3), log10(24.2), log10(4.7), log10(28.9), log10(84.0), log10(11.1), log10(69.0), log10(200.0)},
+    std::vector<double> d = {1, 2, 3, 1, 2, 3, 1, 2, 3},
+                        s = {0.001, 0.001, 0.001, 0.01, 0.01, 0.01, 0.05, 0.05, 0.05},
+                        f = {1.4, 8.3, 24.2, 4.7, 28.9, 84.0, 11.1, 69.0, 200.0},
                         rhs = {0, 0, 0}, vals;
                                             
     
@@ -40,7 +41,7 @@ int main()
     // prove gaussian is correct
     for (int i = 0; i  < 9; i++)
         cout << "Confirming Experiment " << i + 1 << " = " 
-             <<  pow(10, F_log(a, b, c, d[i], s[i])) << endl;
+             <<  F(a, b, c, d[i], s[i]) << endl;
     
     // solve f at given values for d and s
     cout << "F = " << F(a, b, c, 2.5, 0.025) << " at D = 2.5 and S = 0.025" << endl;
@@ -52,41 +53,28 @@ void multiple_linear_regression(std::vector< std::vector<double>>& lhs,
                                 std::vector<double>& rhs, std::vector<double>& d, 
                                 std::vector<double>& s, std::vector<double>& f)
 {
-    double x_1 = 0, x_2 = 0, x2_1 = 0, x2_2 = 0, 
-           x12 = 0, y = 0, yx1 = 0, yx2 = 0;
+    double x1 = 0, x2 = 0, y = 0;
 
     lhs[0][0] = d.size();
     for (int i = 0; i < d.size(); i++)
     {
         // get x1, x2 and y values
-        x_1 = d[i];
-        x2_1 = x_1 * x_1;
-        x_2 = s[i];
-        x2_2 = x_2 * x_2;
-        x12 = x_1 * x_2;
-        y = f[i];
-        yx1 = y * x_1;
-        yx2 = y * x_2;
+        x1 = log10(d[i]);
+        x2 = log10(s[i]);
+        y = log10(f[i]);
 
         // fill multiple linear regression matrix
-        lhs[0][1] += x_1;
-        lhs[0][2] += x_2;
-        lhs[1][0] += x_1;
-        lhs[1][1] += x2_1;
-        lhs[1][2] += x12;
-        lhs[2][0] += x_2;
-        lhs[2][1] += x12;
-        lhs[2][2] += x2_2;
+        fill_matrix(lhs[0], {0, x1, x2});
+        fill_matrix(lhs[1], {x1, x1 * x1, x1 * x2});
+        fill_matrix(lhs[2], {x2, x1 * x2, x2 * x2});
 
         // fill y matrix
-        rhs[0] += y;
-        rhs[1] += yx1;
-        rhs[2] += yx2;
+        fill_matrix(rhs, {y, y * x1, y * x2});
     }
 }
 
-std::vector<double> gaussian_elimination(std::vector<std::vector<double>> lhs, 
-                                         std::vector<double> rhs)
+std::vector<double> gaussian_elimination(std::vector<std::vector<double>>& lhs, 
+                                         std::vector<double>& rhs)
 {
     double sum = 0;
 
