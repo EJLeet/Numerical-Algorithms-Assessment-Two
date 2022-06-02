6f429b8caac1a6a7950c6f126fea0ecbd70fa5af
Ethan Leet <ethanjleet@gmail.com>
2022-06-02 16:11:50 +1000

    abc working

--- Question-6/main_old.cpp
+++ Question-6/main_old.cpp
@@ -12,17 +12,14 @@ using std::endl;
 
 double f(double ep)
 { return (ep - 1) * (ep - 1) - 1; }
-int N, P;
+int N = 10, P = 6;
 
 void set_position(Eigen::VectorXd& x);
 
 class MinimizationExample
 {
 public:
-    std::vector<double> minima_count;
-    double best = 0;
-    Eigen::VectorXd global_config;
-    int run(Eigen::VectorXd& x)
+    std::tuple<Eigen::VectorXd, int> run(Eigen::VectorXd& x)
     {
         double finalCost;
 
@@ -41,26 +38,16 @@ public:
                                         this,
                                         params);
         /* Report the result. */
-        // std::cout << std::setprecision(4)
-        //           << "================================" << std::endl
-        //           << "L-BFGS Optimization Returned: " << ret << std::endl
-        //           << "Minimized Cost: " << finalCost << std::endl
-        //           << "Optimal Variables: " << std::endl
-        //           << x.transpose() << std::endl;
+        std::cout << std::setprecision(4)
+                  << "================================" << std::endl
+                  << "L-BFGS Optimization Returned: " << ret << std::endl
+                  << "Minimized Cost: " << finalCost << std::endl
+                  << "Optimal Variables: " << std::endl
+                  << x.transpose() << std::endl;
         
-        // comment this out if running part b
-        auto it = std::find_if(minima_count.begin(), minima_count.end(), 
-            [finalCost](double b) { return fabs(finalCost - b) < 1e-6; });
-        if (it == minima_count.end())
-            minima_count.push_back(finalCost);
         
-        if (finalCost < best)
-        {
-            best = finalCost;
-            global_config = x.transpose();
-        }
         
-        return ret;
+        return {x, finalCost};
     }
 
 private:
@@ -130,85 +117,84 @@ private:
 
 int main(int argc, char **argv)
 {
-    int iter = std::atoi(argv[1]);
-    P = std::atoi(argv[2]);
-    N = std::atoi(argv[3]);
-    // /*
-    //     Part A
-    //             */
-    // double ep;
-    // std::array<int, 4> p = {3, 6, 10, 14};
-    // // outfiles for part a graphs
-    // std::ofstream p3("3.txt");
-    // std::ofstream p6("6.txt");
-    // std::ofstream p10("10.txt");
-    // std::ofstream p14("14.txt");
+    /*
+        Part A
+                */
+    double ep;
+    std::array<int, 4> p = {3, 6, 10, 14};
+    // outfiles for part a graphs
+    std::ofstream p3("3.txt");
+    std::ofstream p6("6.txt");
+    std::ofstream p10("10.txt");
+    std::ofstream p14("14.txt");
 
-    // for (auto i : p)
-    // {
-    //     for (double r = 0.6; r <= 2.0; r += 0.001)
-    //     {
-    //         ep = exp(i * (1 - r));
-    //         switch(i)
-    //         {
-    //             case 3 : { p3  << r << " " << f(ep) << endl; break; }
-    //             case 6 : { p6  << r << " " << f(ep) << endl; break; }
-    //             case 10: { p10 << r << " " << f(ep) << endl; break; }
-    //             case 14: { p14 << r << " " << f(ep) << endl; break; }
-    //         }
-    //     }
-    // }
+    for (auto i : p)
+    {
+        for (double r = 0.6; r <= 2.0; r += 0.001)
+        {
+            ep = exp(i * (1 - r));
+            switch(i)
+            {
+                case 3 : { p3  << r << " " << f(ep) << endl; break; }
+                case 6 : { p6  << r << " " << f(ep) << endl; break; }
+                case 10: { p10 << r << " " << f(ep) << endl; break; }
+                case 14: { p14 << r << " " << f(ep) << endl; break; }
+            }
+        }
+    }
 
-    // /*
-    //     Part B
-    //     Set global P values
-    //     Comment out minima count in optimiser
-    //     if you run part b
-    //                                              */
-    // double minima;
-    // MinimizationExample part_b;
-    // cout << "n = " << N << ", p = " << P << endl;
-    // Eigen::VectorXd x = Eigen::VectorXd::Zero(N * 3), x_;
-    // set_position(x);
+    /*
+        Part B
+        Set global P values
+                                */
+    double minima;
+    MinimizationExample part_b;
+    cout << "n = " << N << ", p = " << P << endl;
+    Eigen::VectorXd x = Eigen::VectorXd::Zero(N * 3), x_;
+    set_position(x);
     
-    // cout << "Original particle position (x y z): ";
-    // int count = 0;
-    // for (auto i : x) 
-    // {
-    //     if (count++ % 3 == 0) cout << endl;
-    //     cout << i << "    ";
-    // }
-    // cout << endl;
+    cout << "Original particle position (x y z): ";
+    int count = 0;
+    for (auto i : x) 
+    {
+        if (count++ % 3 == 0) cout << endl;
+        cout << i << "    ";
+    }
+    cout << endl;
 
-    // part_b.run(x);
+    std::tie(x_, minima) = part_b.run(x);
 
-    // count = 0;
-    // for (auto i : x_) 
-    // {
-    //     if (count++ % 3 == 0) cout << endl;
-    //     cout << i << "    ";
-    // }
-    // cout << endl << endl;
+    cout << "Minima = " << minima << endl << "New particle position (x y z): ";
+    count = 0;
+    for (auto i : x_) 
+    {
+        if (count++ % 3 == 0) cout << endl;
+        cout << i << "    ";
+    }
+    cout << endl << endl;
 
 /*
         Part C
         Set global N/P values
                                 */
-    MinimizationExample part_c;
     std::vector<double> minima_count;
-    //int iter = 100000000;
-    for (int t = 0; t < iter; t++)
+    for (int t = 0; t < 1000; t++)
     {
-        if (t % 1000 == 0) cout << "Progress: " <<  t << "/" << iter << endl;
+        MinimizationExample part_c;
         Eigen::VectorXd x2 = Eigen::VectorXd::Zero(N * 3), x2_;
         set_position(x2);
 
-        part_c.run(x2);
+        std::tie(x2_, minima) = part_c.run(x2);
+        cout << "Minima Found = " << minima << endl;
+
+        // add minima to set of minima if not already found
+        auto it = std::find_if(minima_count.begin(), minima_count.end(), 
+            [minima](double b) { return fabs(minima - b) < 1e-6; });
+        if (it == minima_count.end())
+            minima_count.push_back(minima);
     }
     cout << "Count of distinc minima at n(" << N << "), p(" << 
-        P << ") = " << part_c.minima_count.size() << endl
-        << "Global Minima = " << part_c.best << endl
-        << "Atomic Configuration: " << endl << part_c.global_config << endl;
+        P << ") = " << minima_count.size() << endl;
 
     return 0;
    
@@ -216,6 +202,16 @@ int main(int argc, char **argv)
 
 void set_position(Eigen::VectorXd& x)
 {
+    // uniform random number gen
+    // std::random_device rand_dev;
+    // std::mt19937 generator(rand_dev());
+
+    // for (int i = 0; i < N * 3; i++)
+    // {
+    //     std::uniform_real_distribution<double> gen(0, cbrt(N));
+    //     double random = gen(generator);
+    //     x[i] = random;
+    // }
     double d = 2.2 * sqrt(3) * (pow(N, 0.333) - 1) * 
         (0.8 + double(rand()) / double(RAND_MAX));
     for (int i = 0; i < N * 3; i++)
